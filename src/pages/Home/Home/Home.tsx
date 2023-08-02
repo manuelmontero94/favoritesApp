d0ec7f7f7fb53ed14e281a3607375e81742f652d
Manuel Montero <mmonteroc@qubosystems.com>
2023-08-02 00:25:11 -0600

    Se agregan los checkbox

--- src/pages/Home/Home/Home.tsx
+++ src/pages/Home/Home/Home.tsx
@@ -1,11 +1,45 @@
 import { People } from '@/data';
+import { Person } from '@/models';
+import { Checkbox } from '@mui/material';
 import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
-import React from 'react';
+import React, { useState } from 'react';
 
 export interface HomeInterface {}
+
 const Home: React.FC<HomeInterface> = () => {
+	const [selectedPeople, setSelectedPeople] = useState<Person[]>([]);
 	const pageSize = 5;
+
+	const findPerson = (person: Person) => !!selectedPeople.find((p) => p.id === person.id);
+	const filteredPerson = (person: Person) => selectedPeople.filter((p) => p.id !== person.id);
+
+	const handleSelectedChange = (person: Person) => {
+		console.log('selectedPeople', selectedPeople);
+		setSelectedPeople(findPerson(person) ? filteredPerson(person) : [...selectedPeople, person]);
+		console.log('selectedPeople', selectedPeople);
+	};
+
 	const columns = [
+		{
+			field: 'actions',
+			headerName: '',
+			type: 'actions',
+			sortable: false,
+			with: 50,
+			renderCell: (params: GridRenderCellParams) => (
+				<>
+					{
+						<Checkbox
+							size="small"
+							checked={findPerson(params.row)}
+							onChange={() => {
+								handleSelectedChange(params.row);
+							}}
+						/>
+					}
+				</>
+			),
+		},
 		{
 			field: 'name',
 			headerName: 'Name',
@@ -17,14 +51,12 @@ const Home: React.FC<HomeInterface> = () => {
 			field: 'category',
 			headerName: 'Category',
 			flex: 1,
-			minWidth: 150,
 			renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
 		},
 		{
 			field: 'company',
 			headerName: 'Company',
 			flex: 1,
-			minWidth: 150,
 			renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
 		},
 	];
